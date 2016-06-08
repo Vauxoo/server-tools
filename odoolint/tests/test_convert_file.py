778cb0d288bc2b1a477f4133b0c2cc9146754a52
Osval Reyes <osval@vauxoo.com>
2016-06-08 15:43:48 +0000

    [REF] odoolint: s/unachievable/unreachable/g
     $ find . -name "*unachievable*" | sed -e 'p;s/unachievable/unreachable/' | xargs -n2 mv
     $ for arch in `grep -rn 'unachievable' . | tr ":" " " | awk '{print $1}'`;do sed -i 's/unachievable/unreachable/g' $arch; done

--- odoolint/tests/test_convert_file.py
+++ odoolint/tests/test_convert_file.py
@@ -21,8 +21,8 @@ class TestConvertFile(common.TransactionCase):
         self.fdemo = 'demo/partner_category_demo.xml'
         self.fdata = 'data/partner_category_data.xml'
         self.fdata2 = 'data/partner_category_data2.xml'
-        self.funachiev = 'data/partner_category_unachievable_data.xml'
-        self.funachiev2 = 'data/partner_category_unachievable_data2.xml'
+        self.funachiev = 'data/partner_category_unreachable_data.xml'
+        self.funachiev2 = 'data/partner_category_unreachable_data2.xml'
 
     def tearDown(self):
         super(TestConvertFile, self).tearDown()
@@ -41,24 +41,24 @@ class TestConvertFile(common.TransactionCase):
         convert_file(self.cr, self.module, self.fdemo, None, kind='demo')
         convert_file(self.cr, self.module, self.fdata2, None, kind='data')
 
-    def test_30_xml_id_ref_unachievable(self):
-        """Test a xml_id referenced unachievable
+    def test_30_xml_id_ref_unreachable(self):
+        """Test a xml_id referenced unreachable
         """
         imd_before = self.imd.search([('module', '=', self.module)])
         convert_file(self.cr, self.module, self.fdemo, None, kind='data')
         imd_after = self.imd.search([('module', '=', self.module)])
         imd_new = (imd_after - imd_before)
-        imd_new.write({'module': 'unachievable'})
+        imd_new.write({'module': 'unreachable'})
         convert_file(self.cr, self.module, self.funachiev, None, kind='data')
 
-    def test_40_xml_id_overwritten_unachievable(self):
-        """Test a xml_id overwritten unachievable
+    def test_40_xml_id_overwritten_unreachable(self):
+        """Test a xml_id overwritten unreachable
         """
         imd_before = self.imd.search([('module', '=', self.module)])
         convert_file(self.cr, self.module, self.fdemo, None, kind='data')
         imd_after = self.imd.search([('module', '=', self.module)])
         imd_new = (imd_after - imd_before)
         self.imm.search([('name', '=', self.module)], limit=1).copy(
-            {'name': 'unachievable', 'state': 'installed'})
-        imd_new.write({'module': 'unachievable'})
+            {'name': 'unreachable', 'state': 'installed'})
+        imd_new.write({'module': 'unreachable'})
         convert_file(self.cr, self.module, self.funachiev2, None, kind='data')
