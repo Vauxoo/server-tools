a8fbeb971e6402a436220d33433e0ac01a7f9cb2
Moisés López <moylop260@vauxoo.com>
2016-08-26 02:05:34 -0500

    Revert "[REF] odoolint: Travis show -1 not +1"
    This reverts commit ff22f982bbbde5002c35270e51d293fc90f35324.

--- odoolint/tests/test_werkzeug_404.py
+++ odoolint/tests/test_werkzeug_404.py
@@ -46,5 +46,5 @@ class TestWerkzeug404(HttpCase):
 
     def test_werkzeug_404(self):
         self.phantom_js('/no_exists', "console.log('ok')", "console")
-        # TODO: Why locally I see 2 but travis see 1
+        # TODO: Why locally I see 1 but travis 2
         self.assertTrue(len(self.__logger_filter.buffer) in [1, 2])
