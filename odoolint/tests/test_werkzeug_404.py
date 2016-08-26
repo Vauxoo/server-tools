ff22f982bbbde5002c35270e51d293fc90f35324
Moisés López <moylop260@vauxoo.com>
2016-08-26 01:45:19 -0500

    [REF] odoolint: Travis show -1 not +1

--- odoolint/tests/test_werkzeug_404.py
+++ odoolint/tests/test_werkzeug_404.py
@@ -45,5 +45,5 @@ class TestWerkzeug404(HttpCase):
 
     def test_werkzeug_404(self):
         self.phantom_js('/no_exists', "console.log('ok')", "console")
-        # TODO: Why locally I see 1 but travis 2
+        # TODO: Why locally I see 2 but travis see 1
         self.assertTrue(len(self.__logger_filter.buffer) in [1, 2])
