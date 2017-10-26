1672aa033c24da30a858d46302e9445bb52cea45
Katherine Zaoral <kathy@vauxoo.com>
2017-10-26 15:48:46 +0000

    [FIX] update model name from ir.actions.report.xml to ir.actions.report
    This model change its name in 11.0. When try to start server a warning
    about this model does not exist and a critical error about the related
    field will not let to start the Odoo server

--- module_prototyper/models/module_prototyper.py
+++ module_prototyper/models/module_prototyper.py
@@ -184,7 +184,7 @@ class ModulePrototyper(models.Model):
               'want to export in this module.')
     )
     report_ids = fields.Many2many(
-        'ir.actions.report.xml', 'prototype_report_rel',
+        'ir.actions.report', 'prototype_report_rel',
         'module_prototyper_id', 'report_id', 'Reports',
         help=('Enter the list of reports that you have created and '
               'want to export in this module.')
