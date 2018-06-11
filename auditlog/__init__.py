e2fa44ddcc04a572a4e340c3342fe138dd280fdf
cubells <vicent@vcubells.net>
2018-06-11 17:26:25 +0200

    [MIG] base_search_fuzzy: Migration to 11.0

--- auditlog/__init__.py
+++ auditlog/__init__.py
@@ -1,5 +1,10 @@
+<<<<<<< HEAD:auditlog/__init__.py
 # -*- coding: utf-8 -*-
 # © 2015 ABF OSIELL <http://osiell.com>
+=======
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+>>>>>>> 1163574e... [MIG] base_search_fuzzy: Migration to 11.0:base_search_fuzzy/__init__.py
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
